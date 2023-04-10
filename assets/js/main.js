29be772d6673c3f2fc404c7f7f1299d487586888
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2023-04-10 12:40:05 -0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+const PRECIO = 400000;
+
+
+const btn = document.querySelector(".btn");
+const cantidad = document.querySelector("#amount");
+const inputColor = document.querySelector ("#inputColor");
+
+const totalAmount = document.querySelector("#totalAmount")
+const colors = document.querySelector("#colorBall")
+
+btn.addEventListener('click', () => {
+const total = cantidad.value * PRECIO;
+const resultado = document.querySelector("#total");
+resultado.innerHTML = total;
+
+totalAmount.innerHTML = cantidad.value;
+colors.style.backgroundColor = inputColor.value;
+
+});
